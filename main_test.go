563c46cdb398b2164b26ae842a545b77460ad799
yangyanan <yyn1110@163.com>
2017-04-04 23:35:50 +0800

    add

---
+++ main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+type parseNameCase struct {
+	str string
+	exp []string
+}
+
+func (testcase parseNameCase) CheckResult(result []string) bool {
+	if len(result) != len(testcase.exp) {
+		return false
+	}
+	for index, str := range result {
+		if str != testcase.exp[index] {
+			return false
+		}
+	}
+	return true
+}
+
+func Test_parseName(t *testing.T) {
+	testcases := []parseNameCase{
+		parseNameCase{"test_table", []string{"test", "table"}},
+		parseNameCase{"testTable", []string{"test", "table"}},
+	}
+
+	for _, testcase := range testcases {
+		result := parseName(testcase.str)
+		if !testcase.CheckResult(result) {
+			t.Errorf("Test %s\nExp: %v\nGot: %v\n", testcase.str, testcase.exp, result)
+		}
+	}
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./table2class -DbAddr=121.40.188.71:3306 -DbName=kubauser -DbUser=yangyanan -DbPassword=khds1234 -OutputPath=./

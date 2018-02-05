a551b18330cf28344448c56b44d85c6ca2562e3e
yangyanan <yangyanan@111.com.cn>
2018-02-05 17:01:49 +0800

    add

--- run-yc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-
-
-./javaPlugin -r --packageName=com.yaoex.passport --dbAddr=10.6.168.14:3306 --dbName=yihaoyaocheng --dbUser=b2btest --dbPassword=b5zUoR4JbcK3emhhUxdL --outputPath=/Users/yangyanan/java/yaocheng-db --dbDriver=druid
--- run-yfy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./javaPlugin  --packageName=com.yao.passport --dbAddr=10.6.82.199:3306 --dbName=mch_new --dbUser=root --dbPassword=QWERasdf1@34 --outputPath=/Users/yangyanan/java/yfy-db --dbDriver=druid
